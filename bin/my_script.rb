687a6a407d48600dbfc6a9c7c64101ea094a0f20
Elif Sezgin <elifmsezgin@gmail.com>
2016-12-05 15:12:22 -0800

    add controller actions & routes for users

--- bin/my_script.rb
+++ bin/my_script.rb
@@ -8,8 +8,44 @@ def index_users
     port: 3000,
     path: '/users.json'
   ).to_s
-
-  puts RestClient.get(url)
+  begin
+    puts RestClient.get(url)
+  rescue RestClient::Exception
+    # something
+  end
 end
 
-index_users
+def create_user(name, email)
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: "/users.json"
+  ).to_s
+
+  puts RestClient.post(url,
+  {
+    user:
+    {
+      name: name, email: email
+    }
+  })
+
+end
+
+def update_user(id, name, email)
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: "/users/#{id}/"
+  ).to_s
+
+  puts RestClient.patch(url,
+  {
+    user:
+    {
+      name: name, email: email
+    }
+  })
+end
